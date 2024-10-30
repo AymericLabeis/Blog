690c3cf9bd43c08f6711d4bf158c37db466329fe
aymeric <aymeric789.labeis@gmail.com>
2024-10-30 19:01:17 +0100

    first commit

---
+++ blog1/members.js
@@ -0,0 +1,13 @@
+const membre = [
+    { id: 1, name: "Alice Johnson", description: "Project manager with 5 years of experience in the tech industry.",
+     profilePicture: "ressources/photo2.jpg", status: "active" },
+    { id: 2, name: "Bob Smith", description: "Senior developer specializing in backend technologies.",
+     profilePicture: "ressources/photo6.jpg", status: "inactive" },
+    { id: 3, name: "Charlie Brown", description: "Marketing expert focused on digital campaigns and social media strategies.",
+     profilePicture: "ressources/photo4.jpg", status: "active" },
+    { id: 4, name: "Diana White", description: "UI/UX designer with a passion for creating user-friendly interfaces.",
+     profilePicture: "ressources/photo3.jpg", status: "inactive" },
+    { id: 5, name: "Evan Green", description: "Data analyst with a background in statistical modeling and machine learning.",
+     profilePicture: "ressources/photo1.jpg", status: "active" },
+     
+  ];
