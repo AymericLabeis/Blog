74c025792ffad4af3d199c79cb36146b0777eb72
aymeric <aymeric789.labeis@gmail.com>
2024-11-01 16:21:38 +0100

    add chat bot

---
+++ blogs.js
@@ -0,0 +1,7 @@
+let blogs = [
+    { id: 1, type: "Technology", title: "AI in Daily Life", description: "Explore AI", photo: "ressources/techno.jpg", rate: 4.8 },
+    { id: 2, type: "Travel", title: "Hidden Gems", description: "Discover Europe", photo: "ressources/travel.jpg", rate: 4.5 },
+    { id: 3, type: "Health & Wellness", title: "5 Simple Ways to Boost Your Mental Health", description: "Tips and techniques to improve your mental well-being and reduce stress in daily life.", photo: "ressources/health.jpg", rate: 4.7 },
+    { id: 4, type: "Finance", title: "Beginner's Guide to Investing", description: "Learn the basics of investing, including key concepts and how to start building a portfolio.", photo: "ressources/finance.jpg", rate: 4.6 },
+    { id: 5, type: "Lifestyle", title: "How to Create a Minimalist Home", description: "Explore the principles of minimalist design and how to declutter your space for a simpler life.", photo: "ressources/lifestyle.jpg", rate: 4.9 }
+  ];
